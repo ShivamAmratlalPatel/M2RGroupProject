be3bc5af3ca81f8d3e26ea981050e06a244a7594
Shivam Patel <shivam.patel19@imperial.ac.uk>
2021-05-27 11:57:26 +0100

    Added in comparison testing

--- main/main.py
+++ main/main.py
@@ -1,4 +1,21 @@
 """The main file for testing."""
-from strategies import random_strategy
+from strategies import random_strategy, strategy1, strategy2
+from statistics import mean
 
-print(random_strategy(number_of_machines=5, number_of_trials=100))
+machine_no = 10
+trial_no = 1000
+
+random_strat = []
+strat1 = []
+strat2 = []
+
+number_of_iterations = 1000
+
+for i in range(number_of_iterations):
+    random_strat.append(random_strategy(machine_no, trial_no))
+    strat1.append(strategy1(machine_no, trial_no))
+    strat2.append(strategy2(machine_no, trial_no))
+
+print(mean(random_strat))
+print(mean(strat1))
+print(mean(strat2))
