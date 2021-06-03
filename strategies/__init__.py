061b30ecd34de41fd7ee323a3b8bea909a5588f9
ShivamAmratlalPatel <sap119@ic.ac.uk>
2021-06-03 12:38:50 +0100

    Changed __init__.py file import style

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,5 +1,5 @@
-from .epsilon_first import *  # noqa F401
-from .random_strategy import *  # noqa F401
-from .strategy2 import *  # noqa F401
-from .thompson_sampling import *  # noqa F401
-from .ucb import *  # noqa F401
+from .epsilon_first import epsilon_first_strategy  # noqa F401
+from .random_strategy import random_strategy_calculator  # noqa F401
+from .strategy2 import strategy2  # noqa F401
+from .thompson_sampling import thompson_sampling_strategy  # noqa F401
+from .ucb import ucb_strategy  # noqa F401
