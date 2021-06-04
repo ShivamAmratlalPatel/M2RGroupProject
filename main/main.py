e81b419deed56a40c35a48755f82e8865a779cf5
ShivamAmratlalPatel <sap119@ic.ac.uk>
2021-06-04 09:28:20 +0100

    Using gaussian method.

--- main/main.py
+++ main/main.py
@@ -6,7 +6,8 @@ from bandits import average_finder
 from strategies import *
 
 machine_no = 100
-trial_no = 1000
+trial_no = 10000
+gaussian = True
 
 random_strategy_regret = []
 epsilon_first_strategy_regret = []
@@ -14,16 +15,20 @@ ucb_strategy_regret = []
 thompson = []
 strategy_list = ["random", "epsilon_first", "ucb", "thompson"]
 
-number_of_iterations = 10
+number_of_iterations = 5
 
 for i in range(number_of_iterations):
     random_strategy_regret.append(
-        random_strategy_calculator(machine_no, trial_no).regret)
+        random_strategy_calculator(machine_no, trial_no, gaussian).regret)
     epsilon_first_strategy_regret.append(
-        epsilon_first_strategy(machine_no, trial_no).regret)
+        epsilon_first_strategy(number_of_machines=machine_no,
+                               number_of_trials=trial_no,
+                               epsilon=0.2, gaussian=gaussian).regret)
     ucb_strategy_regret.append(
-        ucb_strategy(machine_no, trial_no, confidence_level=2).regret)
-    thompson.append((thompson_sampling_strategy(machine_no, trial_no)).regret)
+        ucb_strategy(machine_no, trial_no, confidence_level=2,
+                     gaussian=gaussian).regret)
+    thompson.append(
+        (thompson_sampling_strategy(machine_no, trial_no, gaussian)).regret)
 
 random_average = average_finder(random_strategy_regret, trial_no,
                                 number_of_iterations)
@@ -37,6 +42,8 @@ plt.plot(random_average, label="random")
 plt.plot(epsilon_average, label="epsilon")
 plt.plot(ucb_average, label="ucb")
 plt.plot(thompson_average, label="thompson")
+plt.xlabel("Iteration")
+plt.ylabel("Cumulative Regret")
 plt.grid()
 plt.legend()
 plt.show()
