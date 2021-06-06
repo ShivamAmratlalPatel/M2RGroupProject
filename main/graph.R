a281bd31d829fdcacae509ca34894d4cd7cccf81
ASashikumar <adhavan26@gmail.com>
2021-06-06 13:57:23 +0100

    Added in R project

--- main/graph.R
+++ main/graph.R
@@ -3,5 +3,5 @@
 # Created by: Shivam Patel, Adhavan Sashikumar
 # Created on: 06/06/2021
 
-df <- read.csv("data.csv")
-print(df)
+df <- read.csv("C:\\Users\\adhav\\M2RGroupProject\\M2RGroupProject\\main\\data.csv")
+print("hii")
