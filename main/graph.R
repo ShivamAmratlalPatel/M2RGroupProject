353ae4f054c8a2f98f8ba769ef30196ee1111ab7
ShivamAmratlalPatel <sap119@ic.ac.uk>
2021-06-09 21:54:03 +0100

    Removed R related files

--- main/graph.R
+++
@@ -1,7 +0,0 @@
-# Title     : graph
-# Objective : graph data
-# Created by: Shivam Patel, Adhavan Sashikumar
-# Created on: 06/06/2021
-
-df <- read.csv("C:\\Users\\adhav\\M2RGroupProject\\M2RGroupProject\\main\\data.csv")
-print("hii")
